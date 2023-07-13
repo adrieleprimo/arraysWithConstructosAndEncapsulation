7676ce0c3d7fa082a345efda04000e4e5c0c4ec7
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-13 18:25:01 -0300

    Create Negatives.java

---
+++ entities/Negatives.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Negatives {
+	private int number;
+
+	public Negatives() {
+	}
+	public Negatives(int number) {
+		super();
+		this.number = number;
+	}
+	public int numbers() {
+		return number;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
