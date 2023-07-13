67bd819ba0d836931b9345550cd677590e09c98a
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-13 18:24:39 -0300

    Create Height.java

---
+++ entities/Height.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Height {
+	private String name;
+	private int age;
+	private double height;
+	
+	public Height() {
+		
+	}
+
+	public Height(String name, int age, double height) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+
+}
