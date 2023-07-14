d5256f563b35567adcc4dde8f1b9612976eb6784
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-14 19:43:16 -0300

    Create Sum.java

---
+++ entities/Sum.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Sum {
+	private int A;
+	private int B;
+	public Sum(int a2) {
+}
+	public int getA() {
+		return A;
+	}
+	public void setA(int a) {
+		A = a;
+	}
+	public int getB() {
+		return B;
+	}
+	public void setB(int b) {
+		B = b;
+	}
+	public Sum(int a, int b) {
+		super();
+		A = a;
+		B = b;
+	}
+	
+	
+	
+}
