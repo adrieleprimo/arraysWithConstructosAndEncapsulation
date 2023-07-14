8f67da6244b0d7d9701ff3d60aa6df2430d53f5e
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-14 19:41:33 -0300

    Create Larger.java

---
+++ entities/Larger.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Larger {
+	private double number;
+
+	public Larger(double number) {
+		
+		this.number = number;
+	}
+
+	public double getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
