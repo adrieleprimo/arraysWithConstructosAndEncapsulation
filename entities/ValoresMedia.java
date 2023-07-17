790054bebaf13a5a0a91decb8ebb29c9ba5be19f
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-17 10:31:50 -0300

    Create ValoresMedia.java

---
+++ entities/ValoresMedia.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ValoresMedia {
+	private double number;
+	
+	public ValoresMedia() {
+		
+	}
+
+	public ValoresMedia(double number) {
+		super();
+		this.number = number;
+	}
+
+	public double getNumber() {
+		return number;
+	}
+
+	public void setNumber(double number) {
+		this.number = number;
+	}
+	
+}
