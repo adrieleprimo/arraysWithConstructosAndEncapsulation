cde75ae5659f703d5a2c0b9ccce5eccb4c49c4ff
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-17 11:42:14 -0300

    Create ValoresPares.java

---
+++ entities/ValoresPares.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ValoresPares {
+	private double number;
+	
+	public ValoresPares() {
+		
+	}
+
+	public ValoresPares(double number) {
+		super();
+		this.number = number;
+	}
+
+	public double getNumber() {
+		return number;
+	}
+
+	public void setNumber(double number) {
+		this.number = number;
+	}
+	
+}
