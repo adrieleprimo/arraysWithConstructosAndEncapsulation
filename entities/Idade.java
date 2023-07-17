0a510c3c055e85b464ed74ece296ef7e8c668361
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-17 12:59:15 -0300

    Create Idade.java

---
+++ entities/Idade.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Idade {
+	private int age;
+	private String name;
+	public Idade() {
+		
+	}
+	public Idade(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
